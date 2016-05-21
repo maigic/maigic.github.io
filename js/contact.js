cfc08935cd016df70e06ea85d57572bd842e0a8b
hhyqm <hhyqm@qq.com>
2016-05-21 15:13:26 +0800

    格式化JS代码

--- js/contact.js
+++ js/contact.js
@@ -1,23 +1,33 @@
-var Contact = function () {
+var Contact = function ()
+{
 
-    return {
-        
+    return
+    {
+
         //Map
-        initMap: function () {
-			var map;
-			$(document).ready(function(){
-			  map = new GMaps({
-				div: '#map',
-				lat: -13.004333,
-				lng: -38.494333
-			  });
-			   var marker = map.addMarker({
-		            lat: -13.004333,
-					lng: -38.494333,
-		            title: 'Loop, Inc.'
-		        });
-			});
+        initMap : function ()
+        {
+            var map;
+            $(document).ready(function ()
+            {
+                map = new GMaps(
+                    {
+                        div : '#map',
+                        lat : -13.004333,
+                        lng : -38.494333
+                    }
+                    );
+                var marker = map.addMarker(
+                    {
+                        lat : -13.004333,
+                        lng : -38.494333,
+                        title : 'Loop, Inc.'
+                    }
+                    );
+            }
+            );
         }
 
     };
-}();
+}
+();
